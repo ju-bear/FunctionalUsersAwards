c9c9dac34d987bd7674c6f1e6f7c9cf4679c402b
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 16:11:55 +0400

    Fixed Result.map operator (changed the order of arguments) and added wrap function for List

--- FunctionalUsersAwards.Domain/UtilityTypes.fs
+++ FunctionalUsersAwards.Domain/UtilityTypes.fs
@@ -19,8 +19,11 @@ module Result =
                     | Error f, Ok s -> Error f
                     | Error f, Error s -> Error (f @ s)
     let (<*>) f s = apply f s
-    let (<!>) s f = Result.map f s
+    let (<!>) f s = Result.map f s
     
 module Option = 
     let (>>=) s f = Option.bind f s
     let optionToResult error opt = opt |> Option.map Ok |> Option.defaultValue (Error error) 
+    
+module List =
+    let wrap value = [value]
