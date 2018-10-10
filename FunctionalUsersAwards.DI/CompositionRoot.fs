7e3ecb59c19a11d110ca2d60386b8d3febcc6f79
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 23:41:11 +0400

    Added composition root that injects necessary dependencies for functions

---
+++ FunctionalUsersAwards.DI/CompositionRoot.fs
@@ -0,0 +1,16 @@
+module CompositionRoot
+open AdditionalTypes
+open MainTypes
+
+module AwardRoot = 
+    let maxTitleLength = 55
+    let createDefaultAwardId = AwardId.createWithValue
+    let createDefaultAwardTitle = AwardTitle.create (AwardTitle.validate maxTitleLength)
+    let createDefaultAward = Award.create createDefaultAwardId createDefaultAwardTitle
+
+module UserRoot =
+    let maxUsernameLength = 55
+    let createDefaultUserId = UserId.createWithValue
+    let createDefaultUsername = Username.create (Username.validate maxUsernameLength)
+    let createDefaultUser = User.create createDefaultUserId createDefaultUsername
+
