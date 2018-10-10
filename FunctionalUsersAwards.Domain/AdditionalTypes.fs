297c6342146effb01ee7f89e2bb5f51f6aaaf5c8
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 16:44:06 +0400

    Added more generalized errors for User and Award and sorted locations of type definitions so that award related definitions are higher than user related definitions

--- FunctionalUsersAwards.Domain/AdditionalTypes.fs
+++ FunctionalUsersAwards.Domain/AdditionalTypes.fs
@@ -3,11 +3,36 @@ module AdditionalTypes
 open System
 open UtilityTypes
 
+type AwardId = AwardId of Guid
+
+type AwardTitleError = AwardTitleEmptyError | AwardTitleTooLongError of int
+
+type AwardError = AwardTitleError of AwardTitleError
+
 type UserId = UserId of Guid
 
-type UsernameError = UsernameEmptyError | UsernameTooLongError of int
+type UsernameError = UsernameEmptyError | UsernameTooLongError of int 
 
-type AwardTitleError = AwardTitleEmptyError | AwardTitleTooLongError of int 
+type UserError = UsernameError of UsernameError | AwardListError of AwardError list
+
+module AwardId = 
+    let create() = Guid.NewGuid >> AwardId
+    let createWithValue id = AwardId id
+    let getValue (AwardId id) = id
+
+type AwardTitle = AwardTitle of NonEmptyString
+
+module AwardTitle =
+    let validate maxLength str = if NonEmptyString.length str > maxLength
+                                 then NonEmptyString.length str |> AwardTitleTooLongError |> Error
+                                 else Ok str
+                                 
+    let create validate str = NonEmptyString.create str
+                              |> Option.optionToResult AwardTitleEmptyError
+                              |> Result.bind validate
+                              |> Result.map AwardTitle
+                              
+    let getValue (AwardTitle str) = str
 
 module UserId =
     let create() = Guid.NewGuid >> UserId
@@ -28,25 +53,3 @@ module Username =
    
     
     let getValue (Username str) = str
-    
-
-type AwardId = AwardId of Guid
-
-module AwardId = 
-    let create() = Guid.NewGuid >> AwardId
-    let createWithValue id = AwardId id
-    let getValue (AwardId id) = id
-
-type AwardTitle = AwardTitle of NonEmptyString
-
-module AwardTitle =
-    let validate maxLength str = if NonEmptyString.length str > maxLength
-                                 then NonEmptyString.length str |> AwardTitleTooLongError |> Error
-                                 else Ok str
-                                 
-    let create validate str = NonEmptyString.create str
-                              |> Option.optionToResult AwardTitleEmptyError
-                              |> Result.bind validate
-                              |> Result.map AwardTitle
-                              
-    let getValue (AwardTitle str) = str
