90713b7c8401f50498719320d1d7cba189018afa
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 23:33:38 +0400

    Implemented create for User type

--- FunctionalUsersAwards.Domain/MainTypes.fs
+++ FunctionalUsersAwards.Domain/MainTypes.fs
@@ -14,11 +14,28 @@ module Award =
     
     let create createAwardId createAwardTitle (awardDto: AwardDto) = createFromDomain 
                                                                     <!> (awardDto.Id |> createAwardId |> Ok) 
-                                                                    <*> createAwardTitle awardDto.Title 
-                                                                        |> Result.mapError List.wrap
+                                                                    <*> (createAwardTitle awardDto.Title |> Result.mapError (AwardError.AwardTitleError >> List.wrap))
 
 type User = {
     Id: UserId
     Username: Username
     Awards: Award list
 }
+
+module User = 
+    let createFromDomain id username awards = { Id = id; Username = username; Awards = awards }
+    
+    let private processOneAward accum value = match value with
+                                              | Ok v -> accum |> Result.map (fun x -> v :: x)
+                                              | Error err -> accum |> Result.mapError (fun x -> err :: x)
+                                                                        
+    let private createAwardList (createAward: AwardDto -> Result<Award, AwardError>) list = 
+        list 
+        |> List.map createAward 
+        |> List.fold processOneAward (Ok [])
+    
+    let create createUserId createUsername createAward (userDto: UserDto) = 
+        createFromDomain 
+        <!> (userDto.Id |> createUserId |> Ok) 
+        <*> (createUsername userDto.Username |> Result.mapError (UserError.UsernameError >> List.wrap)) 
+        <*> (userDto.Awards |> createAwardList createAward |> Result.mapError (UserError.AwardListError >> List.wrap))
