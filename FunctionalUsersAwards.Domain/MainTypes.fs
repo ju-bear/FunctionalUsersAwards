265fbf771f60622fb334e4bb2e842fae0260b635
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 16:15:12 +0400

    Implemented create for AwardType

--- FunctionalUsersAwards.Domain/MainTypes.fs
+++ FunctionalUsersAwards.Domain/MainTypes.fs
@@ -1,10 +1,21 @@
 module MainTypes
 open AdditionalTypes
+open UtilityTypes
+open Result
+open Dto
 
 type Award = {
     Id: AwardId
     Title: AwardTitle
 }
+
+module Award =
+    let createFromDomain id title = { Id = id; Title = title }
+    
+    let create createAwardId createAwardTitle (awardDto: AwardDto) = createFromDomain 
+                                                                    <!> (awardDto.Id |> createAwardId |> Ok) 
+                                                                    <*> createAwardTitle awardDto.Title 
+                                                                        |> Result.mapError List.wrap
 
 type User = {
     Id: UserId
