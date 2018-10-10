3a72c1f93ceb6de226884fc8c1bdec105cf395e8
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 15:39:42 +0400

    Added create and get functions for UserId and AwardId

--- FunctionalUsersAwards.Domain/AdditionalTypes.fs
+++ FunctionalUsersAwards.Domain/AdditionalTypes.fs
@@ -5,8 +5,18 @@ open UtilityTypes
 
 type UserId = UserId of Guid
 
+module UserId =
+    let create() = Guid.NewGuid >> UserId
+    let createWithValue id = UserId id
+    let getValue (UserId id) = id
+
 type Username = Username of NonEmptyString
 
 type AwardId = AwardId of Guid
+
+module AwardId = 
+    let create() = Guid.NewGuid >> AwardId
+    let createWithValue id = AwardId id
+    let getValue (AwardId id) = id
 
 type AwardTitle = AwardTitle of NonEmptyString
