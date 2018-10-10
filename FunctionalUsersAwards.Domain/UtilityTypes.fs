870ced65611249126ab5715efe4bd303ab680862
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 12:31:27 +0400

    Implemented create and get for NonEmptyString and added some convenience methods for Result and Option types

--- FunctionalUsersAwards.Domain/UtilityTypes.fs
+++ FunctionalUsersAwards.Domain/UtilityTypes.fs
@@ -2,3 +2,23 @@ module UtilityTypes
 
 type NonEmptyString = NonEmptyString of string
 
+module NonEmptyString = 
+    let create str = match str with
+                     | "" | null -> None
+                     | _ -> Some (NonEmptyString str)
+                     
+    let getValue (NonEmptyString str) = str
+    
+module Result =
+    let (>>=) s f = Result.bind f s
+    let apply f s = match f, s with
+                    | Ok f, Ok s -> Ok (f s)
+                    | Ok f, Error s -> Error s
+                    | Error f, Ok s -> Error f
+                    | Error f, Error s -> Error (f @ s)
+    let (<*>) f s = apply f s
+    let (<!>) s f = Result.map f s
+    
+module Option = 
+    let (>>=) s f = Option.bind f s
+    let optionToResult error opt = Option.defaultValue error opt 
