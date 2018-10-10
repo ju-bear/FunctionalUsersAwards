510c2bfc93f77a10b5a448f254fc63022ae6fd99
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-10 15:58:02 +0400

    Added UsernameError type to mark username errors and implemented create and get functions for Username type

--- FunctionalUsersAwards.Domain/AdditionalTypes.fs
+++ FunctionalUsersAwards.Domain/AdditionalTypes.fs
@@ -5,12 +5,28 @@ open UtilityTypes
 
 type UserId = UserId of Guid
 
+type UsernameError = UsernameEmptyError | UsernameTooLongError of int 
+
 module UserId =
     let create() = Guid.NewGuid >> UserId
     let createWithValue id = UserId id
     let getValue (UserId id) = id
 
 type Username = Username of NonEmptyString
+
+module Username = 
+    let validate maxLength str = if NonEmptyString.length str > maxLength 
+                                 then NonEmptyString.length str |> UsernameTooLongError |> Error 
+                                 else Ok str 
+
+    let create validate str = NonEmptyString.create str 
+                              |> Option.optionToResult UsernameEmptyError 
+                              |> Result.bind validate
+                              |> Result.map Username  
+   
+    
+    let getValue (Username str) = str
+    
 
 type AwardId = AwardId of Guid
 
