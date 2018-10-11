d1b9bb89e1a2afd7662c90299784a82ceac90f45
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-11 17:46:06 +0400

    Fixed indentation for processOne function

--- FunctionalUsersAwards.Domain/UtilityTypes.fs
+++ FunctionalUsersAwards.Domain/UtilityTypes.fs
@@ -25,10 +25,10 @@ module Result =
                     | Error err -> Error err
                     
     let private processOne accum value = match value |> snd with
-                                 | Ok v -> accum |> Result.map (fun x -> v :: x)
-                                 | Error err -> match accum with
-                                                | Ok _ -> [value |> fst, err] |> Error
-                                                | Error accumError -> [value |> fst, err] @ accumError |> Error
+                                         | Ok v -> accum |> Result.map (fun x -> v :: x)
+                                         | Error err -> match accum with
+                                                        | Ok _ -> [value |> fst, err] |> Error
+                                                        | Error accumError -> [value |> fst, err] @ accumError |> Error
                                                 
     let flattenList list = List.fold processOne (Ok []) list
     
