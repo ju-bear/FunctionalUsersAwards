957032cd19bc54a93ab0ba8ae8dd0ec5b0855be4
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-11 16:22:48 +0400

    Added 'add' function

--- FunctionalUsersAwards.InMemoryDataSource/AwardDataSource.fs
+++ FunctionalUsersAwards.InMemoryDataSource/AwardDataSource.fs
@@ -16,3 +16,10 @@ let areAwardsInDataSource (awards: AwardDto list) =
     |> List.map (fun (key, value) -> key)
     |> List.filter (fun x -> awardsGuids |> List.contains x)
     |> (fun resultList -> List.length resultList = List.length awardsGuids, awardsGuids |> List.except resultList)
+    
+let add (award: AwardDto) = 
+    if isUnique award 
+    then 
+        Common.awardMap <- Common.awardMap |> Map.add award.Id award
+        Ok award
+    else Error "Award is already in the database" 
