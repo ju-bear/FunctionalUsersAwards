8ac55d0169c76d60ec229d48d61fb9606a683646
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-11 13:30:27 +0400

    Added toDto functions to composition root

--- FunctionalUsersAwards.DI/CompositionRoot.fs
+++ FunctionalUsersAwards.DI/CompositionRoot.fs
@@ -7,10 +7,12 @@ module AwardRoot =
     let createDefaultAwardId = AwardId.createWithValue
     let createDefaultAwardTitle = AwardTitle.create (AwardTitle.validate maxTitleLength)
     let createDefaultAward = Award.create createDefaultAwardId createDefaultAwardTitle
+    let toDto = Award.toDto
 
 module UserRoot =
     let maxUsernameLength = 55
     let createDefaultUserId = UserId.createWithValue
     let createDefaultUsername = Username.create (Username.validate maxUsernameLength)
     let createDefaultUser = User.create User.validateUser createDefaultUserId createDefaultUsername AwardRoot.createDefaultAward
+    let toDto = User.toDto AwardRoot.toDto
 
