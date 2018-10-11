94ad504e607201f64047152913efef65e9fe58ef
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-11 16:32:08 +0400

    Renamed functions in CompositionRoot

--- FunctionalUsersAwards.DI/CompositionRoot.fs
+++ FunctionalUsersAwards.DI/CompositionRoot.fs
@@ -4,19 +4,19 @@ open MainTypes
 
 module AwardRoot = 
     let maxTitleLength = 55
-    let createDefaultAwardId = AwardId.createWithValue
-    let createDefaultAwardTitle = AwardTitle.create (AwardTitle.validate maxTitleLength)
-    let createDefaultAward = Award.create createDefaultAwardId createDefaultAwardTitle
+    let createId = AwardId.createWithValue
+    let createTitle = AwardTitle.create (AwardTitle.validate maxTitleLength)
+    let createAward = Award.create createId createTitle
     let toDto = Award.toDto
 
 module UserRoot =
     let maxUsernameLength = 55
-    let createDefaultUserId = UserId.createWithValue
-    let createDefaultUsername = Username.create (Username.validate maxUsernameLength)
-    let createDefaultUser = User.create User.validateUser createDefaultUserId createDefaultUsername AwardRoot.createDefaultAward
+    let createId = UserId.createWithValue
+    let createUsername = Username.create (Username.validate maxUsernameLength)
+    let createUser = User.create User.validateUser createId createUsername AwardRoot.createAward
     let toDto = User.toDto AwardRoot.toDto
     
 module AwardLogicRoot =
-    let get = AwardLogic.get AwardRoot.createDefaultAward AwardDataSource.get
-    let getById = AwardLogic.getById AwardRoot.createDefaultAward AwardDataSource.getById
+    let get = AwardLogic.get AwardRoot.createAward AwardDataSource.get
+    let getById = AwardLogic.getById AwardRoot.createAward AwardDataSource.getById
     let add = AwardLogic.add AwardRoot.toDto AwardDataSource.isUnique AwardDataSource.add
