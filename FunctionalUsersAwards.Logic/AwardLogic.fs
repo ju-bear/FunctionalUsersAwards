f642701f82213cf1df30ffc70b954866a477e2c3
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-11 13:57:23 +0400

    Fixed get (get all) function. It doesn't need to return option because it can simply return empty list

--- FunctionalUsersAwards.Logic/AwardLogic.fs
+++ FunctionalUsersAwards.Logic/AwardLogic.fs
@@ -4,9 +4,9 @@ open Dto
 open UtilityTypes
 open Option
 
-let get createAward (getAwardDtosFromDataSource: unit -> AwardDto list option) = getAwardDtosFromDataSource() 
-                                                                                  |> Option.map (List.map (fun x -> (x.Id, createAward x)) 
-                                                                                                 >> Result.flattenList)
+let get createAward (getAwardDtosFromDataSource: unit -> AwardDto list) = getAwardDtosFromDataSource 
+                                                                          >> List.map (fun x -> (x.Id, createAward x)) 
+                                                                          >> Result.flattenList
                                                        
 let getById createAward getAwardFromDataSource id = getAwardFromDataSource id |> Option.map createAward 
 
