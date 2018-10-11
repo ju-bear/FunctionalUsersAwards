a321955e18d834a0d2d10bf799d27a1cc52e827d
Rustam Shekhmametyev <shekhmametyev.rustam@gmail.com>
2018-10-11 19:26:49 +0400

    Added GetAwardById to AwardsController

--- FunctionalUsersAwards.RestUI/Controllers/AwardsController.fs
+++ FunctionalUsersAwards.RestUI/Controllers/AwardsController.fs
@@ -14,7 +14,16 @@ type AwardsController() =
     member this.GetAwards() = match AwardLogicRoot.get() with
                               | Ok awards -> this.Ok(awards |> List.map AwardRoot.toDto) :> IActionResult
                               | Error err -> this.Conflict(err) :> IActionResult
-              
+                              
+    [<Route("{id}")>]
+    [<HttpGet>]
+    member this.GetAward(id) = match AwardLogicRoot.getById id with
+                               | Some award -> match award with
+                                               | Ok award -> this.Ok(award |> AwardRoot.toDto) :> IActionResult
+                                               | Error err -> this.Conflict(err) :> IActionResult
+                               | None -> this.NotFound() :> IActionResult
+
+
     [<Route("add")>]
     [<HttpPost>]                                
     member this.AddAward(award: AwardDto) = match AwardRoot.createAward { award with Id = Guid.NewGuid() } with
